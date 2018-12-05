463dfdf3131987cf8f1dc2326e7ac9ea5fa2b24a
Quentin Gans <quentin.gans@outlook.fr>
2018-12-05 12:22:12 +0100

    update

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController {
+	public function home() {
+		return $this->render('home.html.twig');
+	}
+}
