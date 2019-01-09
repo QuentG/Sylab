911c916f0c55cdf46871d5e5cdb82e5ef5241059
Lucas <lucas.consejo@ynov.com>
2019-01-09 14:10:35 +0100

    update route show

--- src/Controller/BuyController.php
+++ src/Controller/BuyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\ProprieteBien;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,8 +10,12 @@ class BuyController extends AbstractController
 {
 	public function buyHome():Response
 	{
+		$repo =  $this->getDoctrine()->getRepository('App:ProprieteBien');
+		$propriete_bien = $repo->findAll();
+		
 		return $this->render('buy.html.twig', [
-			'current_menu' => 'buy_properties'
+			'current_menu' => 'buy_properties',
+			'proprieties' => $propriete_bien
 		]);
 	}
 }
--- src/Controller/ShowController.php
+++ src/Controller/ShowController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\ProprieteBien;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class ShowController extends AbstractController {
-	public function showHome() {
+	public function showHome($id) {
+
+		$repo =  $this->getDoctrine()->getRepository('App:ProprieteBien');
+		$propriete_bien = $repo->find($id);
+
 		return $this->render('show.html.twig', [
-			'current' => "sell_properties"
+			'current' => "sell_properties",
+			'propriety' => $propriete_bien
 		]);
 	}
 }
