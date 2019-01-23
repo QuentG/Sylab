afe0ced1207bde15efed74406c1b392c222d84b4
Lucas <lucas.consejo@ynov.com>
2019-01-23 16:20:53 +0100

    update controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,4 +20,32 @@ class HomeController extends AbstractController
             'proprieties' => $propriete_bien
         ]);
 	}
+
+	/**
+	 * @return Response
+	 */
+	public function buyHome():Response
+	{
+		$repo =  $this->getDoctrine()->getRepository('App:ProprieteBien');
+		$propriete_bien = $repo->findAll();
+		
+		return $this->render('buy.html.twig', [
+			'current_menu' => 'buy_properties',
+			'proprieties' => $propriete_bien
+		]);
+	}
+
+	/**
+	 * @param $id
+	 * @return Response
+	 */
+	public function showHome($id):Response
+	{
+		$repo =  $this->getDoctrine()->getRepository('App:ProprieteBien');
+		$propriete_bien = $repo->find($id);
+
+		return $this->render('show.html.twig', [
+			'propriety' => $propriete_bien
+		]);
+	}
 }
