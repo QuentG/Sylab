1547349111d49f0ff51226b8512788d01458d058
Lucas <lucas.consejo@ynov.com>
2019-02-27 12:11:11 +0100

    update page error

--- src/Controller/ExceptionController.php
+++ src/Controller/ExceptionController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ExceptionController extends AbstractController
@@ -11,8 +12,13 @@ class ExceptionController extends AbstractController
 	/**
 	 * @return Response
 	 */
-	public function showException():Response
+	public function showException(FlattenException $exception):Response
 	{	
-		return $this->render('bundles/TwigBundle/Exception/error404.html.twig');
+
+        $code = $exception->getStatusCode();
+
+		return $this->render('bundles/TwigBundle/Exception/error404.html.twig', [
+            'status_code' => $code
+        ]);
 	}
 }
