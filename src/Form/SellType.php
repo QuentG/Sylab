81c2e33c0dc03ecc5145d4b5b40968f292c53ce2
Quentin Gans <quentin.gans@outlook.fr>
2019-04-10 10:20:02 +0200

    Fix: bug

--- src/Form/SellType.php
+++ src/Form/SellType.php
@@ -14,52 +14,16 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class SellType extends AbstractType
 {
-<<<<<<< HEAD
-=======
     private function getConfig(string $label, $required = true){
         return [
             'label' => $label,
             'required' => $required
         ];
     }
->>>>>>> 32c80673437b8372293c7739257f3455c430ac4a
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-<<<<<<< HEAD
-            ->add('imageFile', VichImageType::class, [
-				'label' => 'Image de la propriété',
-				'required' => false
-			])
-            ->add('name', TextType::class, [
-            	'label' => 'Nom'
-			])
-            ->add('description', TextType::class, [
-            	'label' => 'Description'
-			])
-            ->add('city', TextType::class, [
-            	'label' => 'Ville'
-			])
-            ->add('address', TextType::class, [
-            	'label' => 'Adresse'
-			])
-            ->add('zip_code', TextType::class, [
-            	'label' => 'Code postal'
-			])
-            ->add('nbr_rooms', IntegerType::class, [
-            	'label' => 'Nombre de chambres'
-			])
-            ->add('surface', IntegerType::class,[
-            	'label' => 'Surface'
-			])
-            ->add('nbr_bedrooms', IntegerType::class, [
-            	'label' => 'Nombre de chambres'
-			])
-            ->add('price', IntegerType::class, [
-                'label' => 'Prix'
-            ])
-=======
             ->add('imageFile', VichImageType::class, $this->getConfig("Image de la Propriété", false))
             ->add('name', TextType::class,  $this->getConfig('Nom'))
             ->add('description', TextType::class, $this->getConfig('Description'))
@@ -70,7 +34,6 @@ class SellType extends AbstractType
             ->add('surface', IntegerType::class, $this->getConfig('Surface'))
             ->add('nbr_bedrooms', IntegerType::class, $this->getConfig('Nombre de chambres'))
             ->add('price', MoneyType::class, $this->getConfig('Prix'))
->>>>>>> 32c80673437b8372293c7739257f3455c430ac4a
             ->add('save', SubmitType::class, [
                 'label' => 'Vendre'
             ])
